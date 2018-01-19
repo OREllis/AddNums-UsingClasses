f8cf0dc485e456fb4c36712578ef687ff6373cd1
30055525 <30055525@cityofglacol.ac.uk>
2018-01-19 11:20:08 +0000

    Added notes to code

--- Add2NumsUsingClasses/Form1.cs
+++ Add2NumsUsingClasses/Form1.cs
@@ -19,25 +19,35 @@ namespace Add2NumsUsingClasses
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
+            //declare class file as doCalcs
             Calcs doCalcs = new Calcs();
 
+            //code to check for errors
             try {
+                //parse textboxes as int, pass them to calcSum in Calcs, assign returned int to int sum
                 int sum = doCalcs.calcSum(int.Parse(txtNo1.Text), int.Parse(txtNo2.Text), int.Parse(txtNo3.Text));
+
+                //pass sum to calcAvg in Calcs, convert returned val to string, set lblAvg text to coverted val
                 lblAvg.Text = doCalcs.calcAvg(sum).ToString("F2");
+
+                //convert sum to string, set lblSum text to converted sum
                 lblSum.Text = sum.ToString();
             }
             catch {
+                //show error if invalid entry
                 MessageBox.Show("Err, enter whole numbers in the textboxes", "Err. Invalid Datatype");
             }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //set textboxes and label text to " "
             txtNo1.Text = txtNo2.Text = txtNo3.Text = lblAvg.Text = lblSum.Text = " ";
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
+            //exit program
             Application.Exit();
         }
 
