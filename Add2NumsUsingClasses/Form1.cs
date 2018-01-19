dd4b8e3087b1778c59aaacb849914ad663c49d37
30055525 <30055525@cityofglacol.ac.uk>
2018-01-19 11:42:14 +0000

    changed displayDetails() name to displayVals()
    changed/removed redundant/vague notes

--- Add2NumsUsingClasses/Form1.cs
+++ Add2NumsUsingClasses/Form1.cs
@@ -19,43 +19,41 @@ namespace Add2NumsUsingClasses
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
-            //declare class file as doCalcs
             Calcs doCalcs = new Calcs();
 
             //code to check for errors
             try {
-                //parse textboxes as int, pass them to calcSum in Calcs, assign returned int to int sum
+                //parse textboxes as int, pass them to calcSum, assign returned value to sum
                 int sum = doCalcs.calcSum(int.Parse(txtNo1.Text), int.Parse(txtNo2.Text), int.Parse(txtNo3.Text));
 
-                //pass sum to calcAvg in Calcs, assign returned val to avg
+                //pass sum to calcAvg, assign returned val to avg
                 double avg = doCalcs.calcAvg(sum);
 
-                //pass sum & avg to method that sets labels to passed values
-                displayDetails(sum, avg);
+                //pass sum & avg to method that sets label text to passed values
+                displayVals(sum, avg);
             }
             catch {
-                //show error if invalid entry
+                //show error message
                 MessageBox.Show("Err, enter whole numbers in the textboxes", "Err. Invalid Datatype");
             }
         }
 
-        public void displayDetails(int sumTotal, double average) {
-            //set lblSum.Text to .ToString(); converted int sumTotal
+        public void displayVals(int sumTotal, double average) {
+            //set lblSum.Text to converted parameter sumTotal
             lblSum.Text = sumTotal.ToString();
 
-            //set lblAvg to .ToString("F2"); converted double average
+            //set lblAvg to converted parameter average
             lblAvg.Text = average.ToString("F2");
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            //set textboxes and label text to " "
+            //clears text
             txtNo1.Text = txtNo2.Text = txtNo3.Text = lblAvg.Text = lblSum.Text = " ";
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            //exit program
             Application.Exit();
         }
 
